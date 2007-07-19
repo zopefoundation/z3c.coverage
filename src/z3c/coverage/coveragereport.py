28c91980258c705b08027c31bd280fc690fb1b6f
Marius Gedminas <marius@gedmin.as>
2007-07-19 15:47:33 +0000

    Make the script directly executable.

--- src/z3c/coverage/coveragereport.py
+++ src/z3c/coverage/coveragereport.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python
 ##############################################################################
 #
 # Copyright (c) 2007 Zope Foundation and Contributors.
