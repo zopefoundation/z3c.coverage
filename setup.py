85bd50f8db22f4b3fa5081a3fa469ddb29d45162
Stephan Richter <stephan.richter@gmail.com>
2007-09-26 21:25:32 +0000

    Got the meta-data wrong.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 
 setup (
     name='z3c.coverage',
-    version='1.0.0',
+    version='1.0.1',
     author = "Zope Community",
     author_email = "zope3-dev@zope.org",
     description = "A script to visualize coverage reports via HTML",
