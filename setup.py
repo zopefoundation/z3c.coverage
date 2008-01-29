5d779a795140ba188e418087d8ed42981f9e6ca6
Stephan Richter <stephan.richter@gmail.com>
2008-01-29 18:27:16 +0000

    version bump.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 
 setup (
     name='z3c.coverage',
-    version='1.1.0',
+    version='1.1.1dev',
     author = "Zope Community",
     author_email = "zope3-dev@zope.org",
     description = "A script to visualize coverage reports via HTML",
