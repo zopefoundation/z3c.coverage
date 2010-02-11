f4e7955f6fbf74de33bc7c7e2badd637804f21d4
Marius Gedminas <marius@gedmin.as>
2010-02-11 09:38:18 +0000

    Rename the main script to coveragereport, to avoid clashes with coverage.
    Bump version number to 1.2.0dev.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 
 setup (
     name='z3c.coverage',
-    version='1.1.4dev',
+    version='1.2.0dev',
     author = "Zope Community",
     author_email = "zope3-dev@zope.org",
     description = "A script to visualize coverage reports via HTML",
@@ -64,7 +64,7 @@ setup (
         ],
     entry_points = """
         [console_scripts]
-        coverage = z3c.coverage.coveragereport:main
+        coveragereport = z3c.coverage.coveragereport:main
         coveragediff = z3c.coverage.coveragediff:main
         """,
     zip_safe = False,
