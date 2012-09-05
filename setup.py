034eb6b435a493ee8b44ff543c3e05a93381b2a8
Marius Gedminas <marius@gedmin.as>
2012-09-05 13:01:56 +0000

    PEP-8-ify setup.py.

--- setup.py
+++ setup.py
@@ -21,12 +21,12 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-setup (
+setup(
     name='z3c.coverage',
-    version = '1.2.1dev',
-    author = "Zope Community",
-    author_email = "zope3-dev@zope.org",
-    description = "A script to visualize coverage reports via HTML",
+    version='1.2.1dev',
+    author="Zope Community",
+    author_email="zope3-dev@zope.org",
+    description="A script to visualize coverage reports via HTML",
     long_description=(
         read('README.txt')
         + '\n\n' +
@@ -39,9 +39,9 @@ setup (
         + '\n\n' +
         read('CHANGES.txt')
         ),
-    license = "ZPL 2.1",
-    keywords = "zope3 test coverage html",
-    classifiers = [
+    license="ZPL 2.1",
+    keywords="zope3 test coverage html",
+    classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
@@ -51,21 +51,21 @@ setup (
         'Operating System :: OS Independent',
         'Topic :: Internet :: WWW/HTTP',
         'Framework :: Zope3'],
-    url = 'http://pypi.python.org/pypi/z3c.coverage',
-    packages = find_packages('src'),
-    include_package_data = True,
-    package_dir = {'':'src'},
-    namespace_packages = ['z3c'],
-    extras_require = dict(
-        test = ['zope.testing'],
+    url='http://pypi.python.org/pypi/z3c.coverage',
+    packages=find_packages('src'),
+    include_package_data=True,
+    package_dir={'': 'src'},
+    namespace_packages=['z3c'],
+    extras_require=dict(
+        test=['zope.testing'],
         ),
-    install_requires = [
+    install_requires=[
         'setuptools',
         ],
-    entry_points = """
+    entry_points="""
         [console_scripts]
         coveragereport = z3c.coverage.coveragereport:main
         coveragediff = z3c.coverage.coveragediff:main
         """,
-    zip_safe = False,
-    )
+    zip_safe=False,
+)
