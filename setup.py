6b82ececa20541d0f446ee6f82f6c06fd9409907
Stephan Richter <stephan.richter@gmail.com>
2013-02-20 05:35:41 +0000

    Back to development: 2.0.1

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def read(*rnames):
 
 setup(
     name='z3c.coverage',
-    version='2.0.0',
+    version='2.0.1.dev0',
     author="Zope Community",
     author_email="zope3-dev@zope.org",
     description="A script to visualize coverage reports via HTML",
